bcb0af5c11c30f44eba0c7af9d84d9a546771890
argherna <argherna>
2010-01-03 20:04:30 +0000

    PerPortletMemento: - added javadoc
    DefaultPerPortletMemento:
    - added javadoc
    - converted bridgeRequest Map to ConcurrentMap
    - removed synchronized blocks around bridgeRequest manipulation

--- portletbridge-ng/portletbridge-core/src/main/java/org/portletbridge/portlet/PerPortletMemento.java
+++ portletbridge-ng/portletbridge-core/src/main/java/org/portletbridge/portlet/PerPortletMemento.java
@@ -23,12 +23,66 @@ import javax.portlet.RenderRequest;
 import org.portletbridge.ResourceException;
 
 /**
+ * A per user memento of preference values and Http state.
+ * 
  * @author JMcCrindle
  */
 public interface PerPortletMemento extends HttpClientState {
+
+
+    /**
+     * Converts PortletPreference values into values that are used by a single
+     * user.  The preference values converted by this method depends on the
+     * implementation of this method.  This could include but not be limited to
+     * {@code initUrl} and {@code scope}
+     *
+     * @param request the RenderRequest
+     *
+     * @throws ResourceException if there's a problem converting the preference
+     * values.
+     */
     void setPreferences(RenderRequest request) throws ResourceException;
+
+
+    /**
+     * Returns a Pattern object usually constructed from the value of the {@code
+     * scope} preference. The returned Pattern is used for deciding whether a 
+     * URL should be rewritten or not i.e. does the URL fall under the scope of 
+     * this portlet or is it an external URL. If the pattern matches, it is
+     * considered in scope.
+     *
+     * @return a Pattern
+     */
     Pattern getScope();
+
+
+    /**
+     * Returns the value of the {@code initUrl} as a {@link java.net.URI URI}.
+     *
+     * This URI could be modified by an implementation of {@link InitUrlFactory}
+     * before it is returned.
+     *
+     * @return the initUrl as a URI.
+     */
     URI getInitUrl();
+
+
+    /**
+     * Caches content fetched from a downstream source.  Content may or may not
+     * be retained when it is fetched from the backing cache.
+     *
+     * @param bridgeRequestId the ID for the given content.
+     * @param content the content to cache.
+     */
     void enqueueContent(String bridgeRequestId, PortletBridgeContent content);
+
+
+    /**
+     * Retrieves the content with the given ID.  Content may or may not be
+     * retained when it is fetched from the backing cache.
+     *
+     * @param bridgeRequestId the ID for the given content.
+     * @return the PortletBridgeContent corresponding to the given ID.
+     */
     PortletBridgeContent dequeueContent(String bridgeRequestId);
 }
